5f2d02e17c37748798714ac25cb759f96c7c9b36
fahad ghazi <42321256+fahad-ghazi@users.noreply.github.com>
2025-06-21 06:38:26 +0300

    Update pull_website_arabic.sh

--- pull_website_arabic.sh
+++ pull_website_arabic.sh
@@ -9,6 +9,5 @@ wget \
 --convert-links \
 --adjust-extension \
 --page-requisites \
---no-parent \
 --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36" \
-https://themodernhouse.com/
+https://mosheer.sa
